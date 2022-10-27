b5a6340e955dfb76b03f905e1037d843085b9b86
Makutamoto <34860673+makutamoto@users.noreply.github.com>
2022-10-27 20:21:02 +0900

    cut line

--- receipt.py
+++ receipt.py
@@ -22,14 +22,14 @@ def receipt(score:int):
     draw.text((0, 30), "Basket-Shooter", font=font5, fill=0)
     draw.text((0, 100), f"{score}", font=font2, fill=0)
     draw.text((300, 195), "point", font=font1, fill=0)
-    draw.text((0, 225), "- - - - - - - - - - - - - -", font=font1, fill=0)
+    draw.text((0, 225), "- - - - キリトリ線 - - - -", font=font1, fill=0)
     draw.text((0, 255), "5E たこ焼き", font=font3, fill=0)
     draw.text((0, 315), "ねぎ・明太マヨ", font=font4, fill=0)
     draw.text((71, 350), "50円割引き！", font=font4, fill=0)
     draw.text((295, 350), "クーポン", font=font6, fill=0)
-    draw.text((295, 368), "使用済印", font=font6, fill=0)
+    draw.text((285, 368), "使用可能印", font=font6, fill=0)
     draw.rectangle((285, 255, 377, 347), width=3)
     return image
 
 if __name__ == '__main__':
-    receipt(100).save('test.png')
+    receipt(100).save('receipt.png')
